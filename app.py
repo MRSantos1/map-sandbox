9cf7105ae372049c6d770af9717a48710f372baa
MRSantos1 <martin.r.santos.1@outlook.com>
2020-01-07 19:01:40 -0800

    app deployment apps

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/map")
+@app.route("/home")
 def index():
     """Return the homepage."""
+    return render_template("index.html")
+
+@app.route("/map")
+def map():
+    """Renders Rental Map"""
     return render_template("map.html")
+
+@app.route("/mhi")
+def mhi():
+    """Renders MHI Graphs"""
+    return render_template("mhi.html")
+
+@app.route("/bar")
+def bar():
+    """Renders MHI Graphs"""
+    return render_template("bar.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
